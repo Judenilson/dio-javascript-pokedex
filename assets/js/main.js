f68f39e5aac34281ba7cf1be8910df216a8c75f8
judenilson <judenilson@hotmail.com>
2022-11-23 20:43:47 -0300

    multiples requests finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,23 @@
+function convertPokemonTypesToLi(pokemonTypes){
+    return pokemonTypes.map((typeSlot) => `<li class= "type">${typeSlot.type.name}</li>`)
+}
+
 function convertPokemonToLi(pokemon) {
     return `
     <li class="pokemon">
-        <span class="number">#001</span>
+        <span class="number">#${pokemon.order}</span>
         <span class="name">${pokemon.name}</span>            
         <div class="detail">
             <ol class="types">
-                <li class="type">grass</li>
-                <li class="type">poison</li>
+                ${convertPokemonTypesToLi(pokemon.types).join('')}
             </ol>
-            <img src="./assets/img/pokeball.png" alt="${pokemon.name}">
+            <img src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
         </div>            
     </li>`
 }
 const pokemonList = document.getElementById('pokemonList')
 
 pokeApi.getPokemons().then((pokemons) => {
-    const listItens = []
-
-    for (let i = 0; i < pokemons.length; i++) {
-        const pokemon = pokemons[i];
-        listItens.push(convertPokemonToLi(pokemon))
-    }
-    
-    console.log(listItens)
-
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
     })
     
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,9 +1,16 @@
 const pokeApi = {}
 
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)        
-        .catch((error) => console.log(error))
+    return fetch(url) //buscando na nossa lista de pokemons
+        .then((response) => response.json()) //pegando a lista de pokemons (response) e convertendo para json
+        .then((jsonBody) => jsonBody.results) //limpando o json p ficar só os resultados de fato, pq a api manda mais coisas, tipo paginação e tal.
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail)) //transformando a lista em uma lista de busca do detalhe, pois o map vai iterar sobre a url de cada pokemon
+        .then((detailRequests) => Promise.all(detailRequests)) //esperando todas as requisições terminarem
+        .then((getPokemonsDetail) => getPokemonsDetail)
 }
+
